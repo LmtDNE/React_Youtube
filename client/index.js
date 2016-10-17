3c492d59e2960de3b602de9174a641e10697e206
Dion <Dion.Fulwood@gmail.com>
2016-10-16 21:13:29 -0700

    Tests youtube-api-search functionality

--- client/index.js
+++ client/index.js
@@ -1,9 +1,14 @@
 //Create new component. This component will produce some HTML
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar.js'
 
 const API_KEY = 'AIzaSyDva9MrZS8JyJpdO0XnIFMkcts2nn1uabk';
+
+YTSearch({key:API_KEY, term: "Solange"}, function(data) {
+  console.log("This is data returned", data);
+})
 // Use this key in your application by passing it with the key=API_KEY parameter.
 
 const App = () => {
